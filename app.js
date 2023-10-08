62a9fa89800ef3d5e27dc627b091b095e723fbf2
Sabdulgafar <salawu.itopa1998@gmail.com>
2023-10-08 15:26:03 +0100

    sending basic interface design

--- app.js
+++ app.js
@@ -1,19 +1,28 @@
 var http = require ("http");
 var express = require("express");
-var app = express();
+var app1 = express();
+var app2 = express();
 
-app.set('view engine', 'ejs');
+app1.set('view engine', 'ejs');
+app2.set('view engine', 'ejs');
 
 http.createServer(function(req,res){
     res.writeHead(200,{'Content-type':'text/plain'});
     res.end('Hello world!');
 }).listen(5000);
 
-app.get('/', function(req,res){
+app1.get('/', function(req,res){
     res.render('page/index');
 })
-app.get('/chat', function(req, res){
+app2.get('/', function(req,res){
+    res.render('page/chatboard');
+})
+app1.get('/chat', function(req, res){
     res.render('page/chat');
 })
-app.listen(8080);
+app2.get('/chat', function(req, res){
+    res.render('page/chatboard');
+})
+app1.listen(8080);
+app2.listen(9090);
 console.log('Loading...')
