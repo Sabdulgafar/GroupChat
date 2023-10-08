812fb72822bb5f241e613589ba4597fba3282969
Sabdulgafar <salawu.itopa1998@gmail.com>
2023-10-08 15:40:30 +0100

    Styling complete

--- model/user.js
+++ model/user.js
@@ -1,24 +1,20 @@
 const mongoose = require("mongoose");
-const bcrypt = require("bcrypt");
-const userSchema = new mongoose.Schema({
-    firstName: {type: String, require: true, trim: true, min: 3, max: 20,},
-    lastName: {type: String, require: true, trim: true, min: 3, max: 20,},
-    username: {type: String, require: true, trim: true, unique: true, lowercase: true, index: true,},
-    email: {type: String, require: true, trim: true, unique: true, lowercase: true,},
-    hash_password: {type: String, require: true,},
-    role: {type: String, enum: ["user", "admin"], default: "user",},
-    contactNumber: {type: String,},
-    profilePicture: {type: String,},
-},{ timestamps: true });
-//For get fullName from when we get data from database
-userSchema.virtual("fullName").get(function () {
-    return `${this.firstName} ${this.lastName}`;
-});
 
-userSchema.method({
-    async authenticate(password) {
-        return bcrypt.compare(password, this.hash_password);
-    },
-});
+const Users = mongoose.Schema({
+    email: {type:String},
+    password:{type:String,require:true},
+    name:{type:String},
+    school:{type:String},
+    phone:{type:String},
+    gender:{type:String},
+    department:{type:String},
+    city:{type:String},
+    bio:{type:String},
+    avater:{type:String},
+    join:  {type:Date, Default:Date.now() },
+    resetPasswordToken: String,
+    resetPasswordExpires:{type:Date},
 
-module.exports = mongoose.model("User", userSchema);
+})
+
+module.exports = mongoose.model('Users', Users);
